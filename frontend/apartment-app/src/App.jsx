85dadec3de52727d26e745c1365d2879e0ae74e8
Nandagopal <nandagopal1999@gmail.com>
2025-04-10 20:31:48 +0530

    Commit to check GIt push working in VS Code

--- frontend/apartment-app/src/App.jsx
+++ frontend/apartment-app/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="text-center p-4">
       <h1 className="text-3xl font-bold text-blue-600">Apartment Management System</h1>
-      <p className="text-gray-700">Welcome to the frontend!</p>
+      <p className="text-gray-700">Welcome to the frontend Git push check!</p>
     </div>
   );
 }
